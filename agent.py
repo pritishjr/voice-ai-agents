f83e9c8415fb702292a87193cac9ff31fa360567
pritishjr <pritishjr.personal@gmail.com>
2026-05-16 12:59:21 +0530

    working voice agent

---
+++ agent.py
@@ -0,0 +1,58 @@
+'''
+This is from the LiveKit tutorials on the basics of building voice ai agents.
+'''
+
+import logging
+from dotenv import load_dotenv
+
+from livekit import agents
+from livekit.agents import JobContext, room_io, Agent, AgentServer, AgentSession
+from livekit.plugins import silero, noise_cancellation
+
+#loading the apis
+load_dotenv()
+
+#handles the behaviour of the agent
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions = "You are a helpful voice agent."
+        )
+        
+#handles dispatching the sessions
+server = AgentServer()
+
+#configuring the voice pipeline:
+
+#this runs when there is an active connection (participant) into the server.#creates an rtc session object and passes onto the entrypoint.
+@server.rtc_session() 
+async def entrypoint(ctx: JobContext):
+    
+    #defining the session pipeline:
+    session = AgentSession(
+        stt = "assemblyai/universal-streaming:en",
+        llm = "openai/gpt-4.1-mini",
+        tts = "cartesia/sonic-3",
+        vad = silero.VAD.load()
+    )
+    
+    #starting the session with noise_cancellation enabled.
+    await session.start(
+        agent=Assistant(),
+        room=ctx.room,
+        room_options=room_io.RoomOptions(
+            audio_input=room_io.AudioInputOptions(
+                noise_cancellation=noise_cancellation.BVC(),  # Background voice cancellation
+            ),
+        ),
+    )
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    agents.cli.run_app(server)
+    
+if __name__ == "__main__":
+    main()
+    
+#working voice agent: avg e2e latency ~3000ms
+
